fca6ea85ef73b760742383f410e0126b5a56d008
Francisco Meza <fmezas@gmail.com>
2017-03-06 19:27:21 -0800

    Initial commit

---
+++ numbers.py
@@ -0,0 +1,16 @@
+symbols = '0123456789ABCDEFGHIJ'
+
+def recurs(number, base):
+    string = symbols[number%base]
+    if number < base:
+        return string
+    else:
+        string = recurs(number/base, base) + string
+        return string
+
+def test(number, base, expected):
+  result = 'PASS' if expected == recurs(number, base) else 'FAIL'
+  print("({0} - {1}, {2}): expected {3}, actual {4}".format(result, number, base, expected, recurs(number, base)))
+
+test(10, 16, 'A')
+test(20, 16, '14')
