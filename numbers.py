1b5d4e4bc7c3bf73e9956c879511b79082d0e9f5
Francisco Meza <fmezas@gmail.com>
2017-03-06 20:42:38 -0800

    nitpicking, consistent indentation

--- numbers.py
+++ numbers.py
@@ -9,8 +9,8 @@ def recurs(number, base):
         return string
 
 def test(number, base, expected):
-  result = 'PASS' if expected == recurs(number, base) else 'FAIL'
-  print("({0} - {1}, {2}): expected {3}, actual {4}".format(result, number, base, expected, recurs(number, base)))
+    result = 'PASS' if expected == recurs(number, base) else 'FAIL'
+    print("({0} - {1}, {2}): expected {3}, actual {4}".format(result, number, base, expected, recurs(number, base)))
 
 test(10, 16, 'A')
 test(20, 16, '14')
